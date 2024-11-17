4beef8dcadb42bde21ab1c7707f28de2e9cd212c
AdamInga1 <145409665+AdamInga1@users.noreply.github.com>
2024-11-16 22:00:56 -0600

    Side bar options
    Add the names of the buttons for the sidebars

--- src/widgets/Sidebar.tsx
+++ src/widgets/Sidebar.tsx
@@ -4,5 +4,21 @@ export function Sidebar():JSX.Element{
 
     return <div className="sidebar">
             <h3>Home</h3>
+          
+            <h5>Reports</h5>
+            <h6>2021</h6>
+            <h6>2022</h6>
+            <h6>2023</h6>
+            <h6>2024</h6>
+            <h6>2025</h6>
+            
+            <h5>Graphs</h5>
+            <h6>2021</h6>
+            <h6>2022</h6>
+            <h6>2023</h6>
+            <h6>2024</h6>
+            <h6>2025</h6>
+            <h6>Comparison</h6>
+            
         </div>
 }
